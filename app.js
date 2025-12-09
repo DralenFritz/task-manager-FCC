cec7462cdcb83ca8a5c57e5c9462677617c968fb
Dralen <oginnisayofunmi12@gmail.com>
2025-12-09 11:25:56 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks.js");
+const connectDB = require("./db/connect.js");
+require("dotenv").config();
+const notFound = require("./middleware/not-found.js");
+const errorHandlerMiddleware = require("./middleware/error-handler.js");
+
+// middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+// routes
+
+app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 4000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
